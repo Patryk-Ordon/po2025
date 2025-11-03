843cccc7c148413f402adfa695f90daddc745177
Patryk Ordon <p.ordon.po@gmail.com>
2025-11-03 18:13:54 +0100

    Zajecia 5: Stworzenie main

---
+++ symulator/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import symulator.*;
+
+public class Main {
+    public static void main(String[] args) {
+        symulator.Samochod mojTraktor = new symulator.Samochod(
+                "TAG8912", "Ursus C330", 24, 2400, 6, 0, 0
+        );
+        mojTraktor.wlacz();
+        mojTraktor.getAktPozycja();
+    }
+}
