914efe5adcf757d19ad7ac29afe50ea893d83c78
Patryk Ordon <p.ordon.po@gmail.com>
2025-11-17 18:12:28 +0100

    SceneBuilder

---
+++ samochodGUI/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.samochodgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.samochodgui to javafx.fxml;
+    exports org.example.samochodgui;
+}
